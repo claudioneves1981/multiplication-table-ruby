5cd06d91892acd4017991899821b20248fffa03a
Claudio Francisco das Neves <cfneguacu@uni9.edu.br>
2022-10-29 15:48:01 -0300

    Multiplication Table Ruby

--- lib/array_utils.rb
+++ lib/array_utils.rb
@@ -1,5 +1,19 @@
 class ArrayUtils
   def self.multiplos(qtd, multiplo)
+    lista = []
+    for i in 0...qtd do
+    lista[i] = (i+1)* multiplo
+    end
+    lista
+  end
 
+  def self.tabuada(qtd)
+   teste = Array.new(qtd) { Array.new(10)}
+   for i in 1..qtd do
+    for j in 1..10 do
+      teste[i-1][j-1] = i * j 
+    end
+   end
+   teste
   end
 end
